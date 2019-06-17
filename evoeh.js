b4153b2df114e9db0f415766ea9ee6479d4f7b1a
JulianaLira <julianadlira@gmail.com>
2019-06-17 20:16:59 -0300

    Adicionando views de Projeto

--- evoeh.js
+++ evoeh.js
@@ -1,5 +1,23 @@
 $(function() {
 	$("#btnenviar").click(function(){
 		alert("Chamou");
+
+
+
+		var dadosjson = {
+			autor: $("#autor").val(),
+			titulo: $("#titulo").val(),
+			assunto: $("#assunto").val(),
+		}
+
+		
 	});
+
+	
+
+
+
+
+
 });
+
